4b7184993a08f7678fcb3a19c8917fab5329e746
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-24 01:10:41 -0500

    Update serverScript.py

--- serverScript.py
+++ serverScript.py
@@ -1,22 +1,40 @@
 import socket
 import threading
 
+clients = {}  # Dictionary to store connected clients
+
 def handle_client(client_socket, username):
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
             if not message:
                 break
-            print(f"{username}> {message}")
+            broadcast(f"{username}> {message}", client_socket)
         except:
             break
+
+def broadcast(message, sender_socket):
+    for client_socket in clients:
+        # Send the message to all clients except the sender
+        if client_socket != sender_socket:
+            try:
+                client_socket.send(message.encode('utf-8'))
+            except:
+                # Remove the client if unable to send the message
+                remove_client(client_socket)
+
+def remove_client(client_socket):
+    if client_socket in clients:
+        username = clients[client_socket]
+        print(f"{username} has left the chat.")
+        del clients[client_socket]
 
 def start_server():
     # Create a socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to a specific address and port
-    server.bind(('10.0.0.171', 5555))
+    server.bind(('your_server_ip', 5555))
 
     # Listen for incoming connections
     server.listen()
@@ -31,6 +49,9 @@ def start_server():
         # Get the username from the connected client
         username = client_socket.recv(1024).decode('utf-8')
         print(f"{username} has joined the chat.")
+
+        # Add the client to the dictionary
+        clients[client_socket] = username
 
         # Create a separate thread to handle the client's messages
         client_thread = threading.Thread(target=handle_client, args=(client_socket, username))
