d24ac8ff1aaf78161946af3fb792725d9d6eeea6
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-24 02:16:40 -0500

    Update clientScript.py

--- clientScript.py
+++ clientScript.py
@@ -1,127 +1,77 @@
 import socket
 import threading
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization, hashes
 
-def receive_messages(client_socket, private_key):
+clients = {}  # Dictionary to store connected clients
+password = "your_password"  # Set your desired password here
+
+def handle_client(client_socket, username):
     while True:
         try:
-            encrypted_message = client_socket.recv(1024)
-            if not encrypted_message:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
                 break
-
-            # Decrypt the received message
-            decrypted_message = private_key.decrypt(
-                encrypted_message,
-                padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                    algorithm=hashes.SHA256(),
-                    label=None
-                )
-            ).decode('utf-8')
-
-            print(decrypted_message)
-        except Exception as e:
-            print(f"Error receiving message: {e}")
+            formatted_message = f"{username}> {message}"
+            print(formatted_message)
+            broadcast(formatted_message, client_socket)
+        except:
             break
 
-def encrypt_message(message, public_key):
-    # Encrypt the message using the recipient's public key
-    encrypted_message = public_key.encrypt(
-        message.encode('utf-8'),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return encrypted_message
+def broadcast(message, sender_socket):
+    for client_socket in clients:
+        # Send the message to all clients except the sender
+        if client_socket != sender_socket:
+            try:
+                client_socket.send(message.encode('utf-8'))
+            except:
+                # Remove the client if unable to send the message
+                remove_client(client_socket)
 
-def decrypt_message(encrypted_message, private_key):
-    # Decrypt the message using the private key
-    decrypted_message = private_key.decrypt(
-        encrypted_message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    ).decode('utf-8')
-    return decrypted_message
+def remove_client(client_socket):
+    if client_socket in clients:
+        username = clients[client_socket]
+        print(f"{username} has left the chat.")
+        del clients[client_socket]
 
-def generate_key_pair():
-    # Generate an RSA key pair for encryption/decryption
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-
-    # Get the public key for sharing with the server
-    public_key = private_key.public_key()
-
-    return private_key, public_key
-
-def start_client():
-    # Get user input for username and password
-    username = input("Enter your username: ")
-    password = input("Enter the server password: ")
-
+def start_server():
     # Create a socket
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect to the server
-    client.connect(('your_server_ip', 5555))
+    # Bind the socket to a specific address and port
+    server.bind(('0.0.0.0', 5555))  # Allow connections from any IP
 
-    # Send the password to the server
-    client.send(password.encode('utf-8'))
+    # Listen for incoming connections
+    server.listen()
 
-    # Receive the server's public key
-    server_public_key_bytes = client.recv(1024)
-    server_public_key = serialization.load_pem_public_key(
-        server_public_key_bytes,
-        backend=default_backend()
-    )
-
-    # Generate an RSA key pair for encryption/decryption
-    client_private_key, client_public_key = generate_key_pair()
-
-    # Send the client's public key to the server
-    client.send(client_public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    ))
-
-    # Receive the server's confirmation message
-    confirmation_message = client.recv(1024)
-    print(decrypt_message(confirmation_message, client_private_key))
-
-    # Create a separate thread to receive messages
-    receive_thread = threading.Thread(target=receive_messages, args=(client, client_private_key))
-    receive_thread.start()
+    print("Server is waiting for connections...")
 
     while True:
-        # Get user input and send messages
-        message = input()
-        if message.lower() == 'exit':
-            break
+        # Accept the connection from a client
+        client_socket, client_address = server.accept()
+        print(f"Connected to {client_address}")
 
-        # Encrypt the message using the server's public key
-        encrypted_message = server_public_key.encrypt(
-            message.encode('utf-8'),
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
-        )
+        # Get the password from the connected client
+        entered_password = client_socket.recv(1024).decode('utf-8')
 
-        # Send the encrypted message to the server
-        client.send(encrypted_message)
+        if entered_password == password:
+            # Password is correct, proceed with user authentication
 
-    # Close the connection when exiting the chat
-    client.close()
+            # Get the username from the connected client
+            username = client_socket.recv(1024).decode('utf-8')
+            print(f"{username} has joined the chat.")
+
+            # Add the client to the dictionary
+            clients[client_socket] = username
+
+            # Send a confirmation message to the client
+            client_socket.send("Welcome to the chat!".encode('utf-8'))
+
+            # Create a separate thread to handle the client's messages
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, username))
+            client_thread.start()
+        else:
+            # Incorrect password, close the connection
+            print("Incorrect password. Closing connection.")
+            client_socket.close()
 
 if __name__ == "__main__":
-    start_client()
+    start_server()
