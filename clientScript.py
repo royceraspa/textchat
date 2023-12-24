3f8cdb93e31d07a51318e5870b09de0cb9a25f23
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-24 03:18:29 -0500

    Update clientScript.py

--- clientScript.py
+++ clientScript.py
@@ -7,7 +7,7 @@ def receive_messages(client_socket, username):
             message = client_socket.recv(1024).decode('utf-8')
             if not message:
                 break
-            print(f"\n{message}\n{username}> ", end="", flush=True)
+            print(f"\r{message}\n{username}> ", end="", flush=True)
         except:
             break
 
@@ -33,8 +33,8 @@ def start_client():
         message = input(f"{username}> ")
         if message.lower() == 'exit':
             break
-        print(f"\r{message}", end="", flush=True)
-        client.send(message.encode('utf-8'))
+        print(f"\033[F\033[K{username}> {message}\n{username}> ", end="", flush=True)
+        client.send(message.encode('utf-8"))
 
     # Close the connection
     client.close()
